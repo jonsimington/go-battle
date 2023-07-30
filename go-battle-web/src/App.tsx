c9e9ea13d6ad073dfeb6035070b4bc2ffea5580c
Jon Simington <jon@simington.io>
2023-07-30 03:45:43 -0500

    Add bootstrap to web project

--- go-battle-web/src/App.tsx
+++ go-battle-web/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FaChessBishop } from "react-icons/fa6";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home"><FaChessBishop></FaChessBishop> Go Battle</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Game History</Nav.Link>
+            <Nav.Link href="#pricing">Create Game</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="body">
+
+      </div>
     </div>
   );
 }
