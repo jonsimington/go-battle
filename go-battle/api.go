808eb720b6f6476e7b8ac9a69a7dab9d380cf784
Jon Simington <jon@simington.io>
2023-08-20 17:01:33 -0500

    Remove Server.go

--- go-battle/api.go
+++ go-battle/api.go
@@ -87,3 +87,35 @@ func main() {
 
 	app.Listen(":3000")
 }
+
+func createGame() {
+	// p1 := Player{
+	// 	"jon",
+	// 	Client{
+	// 		"https://github.com/brianwgoldman/megaminerai-19-stumped",
+	// 		"py",
+	// 		"Stumped",
+	// 	},
+	// }
+
+	// p2 := Player{
+	// 	"adam",
+	// 	Client{
+	// 		"https://github.com/brianwgoldman/megaminerai-19-stumped",
+	// 		"py",
+	// 		"Stumped",
+	// 	},
+	// }
+
+	// m := Match{
+	// 	getCurrentMatchID(db),
+	// 	7,
+	// 	[]Game{},
+	// 	[]Player{
+	// 		p1,
+	// 		p2,
+	// 	},
+	// }
+
+	// m.StartMatch(db)
+}
