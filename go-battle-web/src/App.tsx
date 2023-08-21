c4c49af8287d22326d9d0071120bcc5c10d0eb91
Jon Simington <jon@simington.io>
2023-08-20 22:11:01 -0500

    Add dropdowns in navbar for players, games, matches, clients

--- go-battle-web/src/App.tsx
+++ go-battle-web/src/App.tsx
@@ -2,8 +2,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
-import { FaChessBishop } from "react-icons/fa6";
+import { FaChessBishop, FaPlus, FaMagnifyingGlass } from "react-icons/fa6";
 
 
 function App() {
@@ -14,8 +15,22 @@ function App() {
           <Navbar.Brand href="#home"><FaChessBishop></FaChessBishop> Go Battle</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Game History</Nav.Link>
-            <Nav.Link href="#pricing">Create Game</Nav.Link>
+            <NavDropdown title="Players" id="players-dropdown">
+                <NavDropdown.Item href="#players/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Players</NavDropdown.Item>
+                <NavDropdown.Item href="#players/create"><FaPlus></FaPlus> Create Player</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Clients" id="clients-dropdown">
+                <NavDropdown.Item href="#clients/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Clients</NavDropdown.Item>
+                <NavDropdown.Item href="#clients/create"><FaPlus></FaPlus> Create Client</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Matches" id="matches-dropdown">
+                <NavDropdown.Item href="#matches/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Matches</NavDropdown.Item>
+                <NavDropdown.Item href="#matches/create"><FaPlus></FaPlus> Create Match</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Games" id="games-dropdown">
+                <NavDropdown.Item href="#games/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Games</NavDropdown.Item>
+                <NavDropdown.Item href="#games/create"><FaPlus></FaPlus> Create Game</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
