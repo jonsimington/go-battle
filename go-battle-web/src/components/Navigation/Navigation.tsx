83011c0395400fce1a7c3cb814c140e2a95152cb
Jon Simington <jon@simington.io>
2023-08-27 21:43:08 -0500

    Add link to cerveau in navbar

--- go-battle-web/src/components/Navigation/Navigation.tsx
+++ go-battle-web/src/components/Navigation/Navigation.tsx
@@ -8,33 +8,42 @@ import { FaChessBishop, FaPlus, FaMagnifyingGlass } from "react-icons/fa6";
 
 interface NavigationProps {}
 
-const Navigation: FC<NavigationProps> = () => (
-  <div className={styles.Navigation} data-testid="Navigation">
-    <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-            <Navbar.Brand href="#home"><FaChessBishop></FaChessBishop> Go Battle</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Players" id="players-dropdown">
-                <NavDropdown.Item href="/players/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Players</NavDropdown.Item>
-                <NavDropdown.Item href="/players/create"><FaPlus></FaPlus> Create Player</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Clients" id="clients-dropdown">
-                <NavDropdown.Item href="/clients/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Clients</NavDropdown.Item>
-                <NavDropdown.Item href="/clients/create"><FaPlus></FaPlus> Create Client</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Matches" id="matches-dropdown">
-                <NavDropdown.Item href="/matches/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Matches</NavDropdown.Item>
-                <NavDropdown.Item href="/matches/create"><FaPlus></FaPlus> Create Match</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Games" id="games-dropdown">
-                <NavDropdown.Item href="/games/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Games</NavDropdown.Item>
-                <NavDropdown.Item href="/games/create"><FaPlus></FaPlus> Create Game</NavDropdown.Item>
-            </NavDropdown>
-            </Nav>
-        </Container>
-    </Navbar>
-  </div>
-);
+const Navigation: FC<NavigationProps> = () => {
+    const cerveauUrl = process.env.REACT_APP_CERVEAU_URL;
+
+    return (
+        <div className={styles.Navigation} data-testid="Navigation">
+          <Navbar bg="dark" data-bs-theme="dark">
+              <Container>
+                  <Navbar.Brand href="#home"><FaChessBishop></FaChessBishop> Go Battle</Navbar.Brand>
+                  <Nav className="me-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <NavDropdown title="Players" id="players-dropdown">
+                      <NavDropdown.Item href="/players/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Players</NavDropdown.Item>
+                      <NavDropdown.Item href="/players/create"><FaPlus></FaPlus> Create Player</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Clients" id="clients-dropdown">
+                      <NavDropdown.Item href="/clients/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Clients</NavDropdown.Item>
+                      <NavDropdown.Item href="/clients/create"><FaPlus></FaPlus> Create Client</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Matches" id="matches-dropdown">
+                      <NavDropdown.Item href="/matches/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Matches</NavDropdown.Item>
+                      <NavDropdown.Item href="/matches/create"><FaPlus></FaPlus> Create Match</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Games" id="games-dropdown">
+                      <NavDropdown.Item href="/games/search"><FaMagnifyingGlass></FaMagnifyingGlass> Search Games</NavDropdown.Item>
+                      <NavDropdown.Item href="/games/create"><FaPlus></FaPlus> Create Game</NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href={cerveauUrl}>Cerveau</Nav.Link>
+                  </Nav>
+              </Container>
+          </Navbar>
+        </div>
+    );
+}
+
+
+
+
 
 export default Navigation;
