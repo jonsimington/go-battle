1063fccce9a8be165ccf6138baedd4fd900c7b95
Jon Simington <jon@simington.io>
2023-09-12 23:37:07 -0500

    Adjust table spacing on players page

--- go-battle-web/src/components/Players/SearchPlayers/SearchPlayers.tsx
+++ go-battle-web/src/components/Players/SearchPlayers/SearchPlayers.tsx
@@ -46,9 +46,9 @@ export function SearchPlayers({ tableData, refreshData }: SearchPlayersProps): J
             title: "Name",
             render: (_, { name, ID }) => {
                 return (
-                    <div className="d-flex-inline">
+                    <div className="d-flex">
                         <Button 
-                            className="text-light"
+                            className="text-light flex-grow-1"
                             variant={`outline-secondary`} 
                             size="sm" 
                             key={`player-games-link-${ID}`}
@@ -57,6 +57,7 @@ export function SearchPlayers({ tableData, refreshData }: SearchPlayersProps): J
                         </Button>
                         <div className="float-end">
                             <Button 
+                                className="ms-2"
                                 href={`${window.location.origin}/games/search?players=${ID}`} 
                                 variant={`outline-info`} 
                                 size="sm" 
@@ -194,6 +195,7 @@ export function SearchPlayers({ tableData, refreshData }: SearchPlayersProps): J
         {
             key: "elo_history",
             title: "ELO History",
+            width: 200,
             render: (_, { elo_history }) => {
                 if (elo_history.length === 0) {
                     return "No History Yet";
