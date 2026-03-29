7950e5cd914bc4125452d009be1a48e45f84a064
Jon Simington <jon@simington.io>
2026-03-29 17:02:19 -0500

    Add vis prod dockerfile

--- rebuild_web.sh
+++ rebuild_web.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker-compose stop web && cd go-battle-web/ && sudo rm -rf node_modules/ && npm i && npm run build --production && cd .. && docker-compose -f prod.docker-compose.yml build web && docker-compose -f prod.docker-compose.yml up -d
+docker-compose -f prod.docker-compose.yml stop web viseur \
+  && docker-compose -f prod.docker-compose.yml build web viseur \
+  && docker-compose -f prod.docker-compose.yml up -d
